1c708f09364769a6d0bd5b11e735a7c9309937dd
ABDUS SAMAD SAIFUL <samadsaifullah11@gmail.com>
2023-05-19 14:44:55 +0600

    added jeep toy section

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-app= express();
+app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
@@ -14,7 +14,7 @@ app.use(express.json())
 
 console.log(process.env.DB_PASS)
 
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
     res.send('toy server is running........')
 })
 
@@ -23,48 +23,54 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    //await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        //await client.connect();
 
-    //classic section
-    const classicCollection =client.db('classicCarDB').collection('classic');
+        //classic section
+        const classicCollection = client.db('classicCarDB').collection('classic');
 
-    app.get('/classic', async(req, res)=>{
-        const cursor = classicCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+        app.get('/classic', async (req, res) => {
+            const cursor = classicCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
-    //luxury section
-    const luxuryCollection = client.db('classicCarDB').collection('luxury');
+        //luxury section
+        const luxuryCollection = client.db('classicCarDB').collection('luxury');
 
-    app.get('/luxury', async(req, res)=>{
-        const cursor = luxuryCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+        app.get('/luxury', async (req, res) => {
+            const cursor = luxuryCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
-    //jeep car section
-   
+        //jeep car section
+        const jeepCollection = client.db('classicCarDB').collection('jeep');
+
+        app.get('/jeep', async (req, res) => {
+            const cursor = jeepCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    //await client.close();
-  }
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -74,6 +80,6 @@ run().catch(console.dir);
 
 
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Toy server is running on port: ${port}`);
 })
