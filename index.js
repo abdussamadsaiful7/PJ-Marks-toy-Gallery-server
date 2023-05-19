aa4512bcd0ab6ea6eb158f33f590a2200984803c
ABDUS SAMAD SAIFUL <samadsaifullah11@gmail.com>
2023-05-19 15:04:26 +0600

    added configure for get spicific one id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -42,6 +42,14 @@ async function run() {
             const cursor = classicCollection.find();
             const result = await cursor.toArray();
             res.send(result);
+        })
+
+        app.get('/classic/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await classicCollection.findOne(query);
+            res.send(result);
+
         })
 
         //luxury section
